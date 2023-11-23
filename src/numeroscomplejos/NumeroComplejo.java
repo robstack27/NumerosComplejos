9f027bd95f0c8172ad2255d3142e3fb27f6c167d
robinson <rjquispe1@espe.edu.ec>
2023-11-23 08:49:12 -0500

    Version1

--- src/numeroscomplejos/NumeroComplejo.java
+++ src/numeroscomplejos/NumeroComplejo.java
@@ -1,30 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package numeroscomplejos;
 
-/**
- *
- * @author labctr
- */
 public class NumeroComplejo {
-    private int real=0;
-    private Integer img=0;
+    private double real;
+    private double imaginario;
+
     public void cargar(String numero) {
-        int r=5,i=7;
-       
-        this.real=r;
-        this.img=i;
+        // Separar la cadena en partes real e imaginaria
+        String[] partes = numero.split("\\+");
+        this.real = Double.parseDouble(partes[0]);
+        this.imaginario = Double.parseDouble(partes[1].replace("i", ""));
     }
-    public String mostrar(){
-        
-        return (this.real+""+this.img.toString()+"i");
+
+    public String mostrar() {
+        // Formatear la salida
+        return String.format("%.2f + %.2fi", this.real, this.imaginario);
     }
-    public String suma(NumeroComplejo num){
-        
-        
-        return "";
+
+    public NumeroComplejo suma(NumeroComplejo num) {
+        NumeroComplejo resultado = new NumeroComplejo();
+        resultado.real = this.real + num.real;
+        resultado.imaginario = this.imaginario + num.imaginario;
+        return resultado;
+    }
+
+    public NumeroComplejo resta(NumeroComplejo num) {
+        NumeroComplejo resultado = new NumeroComplejo();
+        resultado.real = this.real - num.real;
+        resultado.imaginario = this.imaginario - num.imaginario;
+        return resultado;
+    }
+
+    public static void main(String[] args) {
+        NumeroComplejo num1 = new NumeroComplejo();
+        num1.cargar("7+4i");
+
+        NumeroComplejo num2 = new NumeroComplejo();
+        num2.cargar("120+50i");
+
+        System.out.println("Número 1: " + num1.mostrar());
+        System.out.println("Número 2: " + num2.mostrar());
+
+        NumeroComplejo suma = num1.suma(num2);
+        System.out.println("Suma: " + suma.mostrar());
+
+        NumeroComplejo resta = num1.resta(num2);
+        System.out.println("Resta: " + resta.mostrar());
     }
 }
